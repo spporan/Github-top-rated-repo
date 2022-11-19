cd4394b6c621db7258198c143f8ba360c85ab008
poran <poran.cse@gmail.com>
2022-11-20 00:34:12 +0600

    refactor AndroidRepoNetworkDataSource.kt

--- app/src/main/java/poran/cse/github_top_rated_repo/data/source/remote/AndroidRepoNetworkDataSource.kt
+++ app/src/main/java/poran/cse/github_top_rated_repo/data/source/remote/AndroidRepoNetworkDataSource.kt
@@ -6,8 +6,11 @@ import javax.inject.Inject
 class AndroidRepoNetworkDataSource @Inject constructor(
     private val githubApi: GithubApiService
 )  {
+    companion object{
+        const val ITEM_PER_PAGE  = 10
+    }
     suspend fun fetchRepos(
         page: Int,
-        itemPerPage: Int
+        itemPerPage: Int = ITEM_PER_PAGE
     ) = githubApi.getAndroidRepos(page, itemPerPage)
 }
