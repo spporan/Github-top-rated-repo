044cb2b04f26782fec05dd6b0e566493908bdb53
poran <poran.cse@gmail.com>
2022-11-20 17:52:19 +0600

    Fix Di related issues for adapter

--- app/src/main/java/poran/cse/github_top_rated_repo/ui/adapter/RepoPagingAdapter.kt
+++ app/src/main/java/poran/cse/github_top_rated_repo/ui/adapter/RepoPagingAdapter.kt
@@ -10,10 +10,11 @@ import poran.cse.github_top_rated_repo.databinding.RepoItemViewBinding
 import poran.cse.github_top_rated_repo.ui.adapter.listeners.RepoClickListener
 import poran.cse.github_top_rated_repo.ui.adapter.viewholders.RepoItemViewHolder
 import poran.cse.github_top_rated_repo.ui.uistates.RepoUiModel
+import javax.inject.Inject
 
-class RepoPagingAdapter(
-    private val onRepoItemClickListener: RepoClickListener
-    ): PagingDataAdapter<RepoUiModel, RecyclerView.ViewHolder>(diffCallback) {
+class RepoPagingAdapter @Inject constructor(): PagingDataAdapter<RepoUiModel, RecyclerView.ViewHolder>(diffCallback) {
+
+    var onRepoItemClickListener: RepoClickListener? = null
 
     companion object {
         private val diffCallback = object : DiffUtil.ItemCallback<RepoUiModel>() {
